692323c90db6529b8997a9036fa2fbd06a2c1747
Rin-t <rin.t.4921@gmail.com>
2023-06-25 17:49:30 +0900

    add: テキストフィールドタップ時に入力された文字列をメソッドに渡す処理を作成

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
@@ -24,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -41,8 +31,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  void updateText() {
-    print('Button clicked!');
+  final TextEditingController _controller = TextEditingController();
+
+  void uploadText(String text) {
+    print(text);
   }
 
   @override
@@ -58,7 +50,8 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
             Container(
               width: 240,
-              child: const TextField(
+              child: TextField(
+                controller: _controller,
                 decoration: InputDecoration(
                     hintText: "入力してね！"
                 ),
@@ -66,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             SizedBox(height: 40),
             ElevatedButton(
-              onPressed: updateText,
+              onPressed: () => uploadText(_controller.text),
               child: Text("送信！"),
             )
           ]
